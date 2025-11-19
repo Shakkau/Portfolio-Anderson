a43cdd547d0c3e38e0c8263f2a753324f44c972c
Anderson <Anderson_Junior2@hotmail.com>
2025-11-19 12:53:10 -0300

    💄 fix: mudança no nome da pasta do css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './css/index.css'
+import './cssTailwind/index.css'
 import AppRoutes from './routes/Routes.tsx'
 
 createRoot(document.getElementById('root')!).render(
